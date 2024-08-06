3027927a9ae9654ee8d591b352782c8d2b6d0b1d
Thabiso <thabisongubanee@gmail.com>
2024-08-06 16:55:02 +0200

    Add project files.

---
+++ DutLecturerBooking/Data/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿namespace DutLecturerBooking.Data
+{
+    public class ApplicationUser
+    {
+        public int UserId {  get; set; }
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
+        public string Facult {  get; set; }
+        public string Department {  get; set; }
+        public string CourseName {  get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
---
+++ DutLecturerBooking/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DutLecturerBooking.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
